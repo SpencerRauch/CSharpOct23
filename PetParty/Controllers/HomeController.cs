1721fbb9824d1799a91687acb1d687b67401d1b6
Spencer Rauch <rauchs@gmail.com>
2023-09-27 13:43:28 -0700

    PetParty started ( first new mvc )

---
+++ PetParty/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PetParty.Models;
+
+namespace PetParty.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
