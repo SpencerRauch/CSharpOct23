2141207a56763dc64b02ee1148e8f73e6a0e9eee
Spencer Rauch <rauchs@gmail.com>
2023-10-06 06:53:50 -0700

    bfs

--- algos/BinarySearchTrees.js
+++ algos/BinarySearchTrees.js
@@ -336,6 +336,18 @@ class BinarySearchTree {
         }
         return 1 + Math.max(this.height(node.left), this.height(node.right));
     }
+
+    /**
+     * BFS order: horizontal rows top-down left-to-right.
+     * Converts this BST into an array following Breadth First Search order.
+     * Example on the fullTree var:
+     * [25, 15, 50, 10, 22, 35, 70, 4, 12, 18, 24, 31, 44, 66, 90]
+     * @param {Node} current The current node during the traversal of this tree.
+     * @returns {Array<number>} The data of all nodes in BFS order.
+     */
+    toArrLevelorder(current = this.root) {
+        
+    }
 }
 
 const emptyTree = new BinarySearchTree();
