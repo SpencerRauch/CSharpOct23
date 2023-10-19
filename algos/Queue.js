2ccc8866669058ac4cc5c40d0a7e0bbc1e5cda15
Spencer Rauch <rauchs@gmail.com>
2023-10-19 07:42:12 -0700

    queue again

--- algos/Queue.js
+++ algos/Queue.js
@@ -213,6 +213,20 @@ function CompareQueues(qOne, qTwo) {
     return isEqual;
 }
 
+/**
+ * Determines whether the sum of the left half of the queue items is equal to
+ * the sum of the right half. Avoid indexing the queue items directly via
+ * bracket notation, use the queue methods instead for practice.
+ * Use no extra array or objects.
+ * The queue should be returned to it's original order when done.
+ * If queue cannot be evenly divided, return false.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @returns {boolean} Whether the sum of the left and right halves is equal.
+ */
+function isSumOfHalvesEqual(queue) {
+    //Your code here
+}
 
 const arrayQueueOne = new Queue();
 arrayQueueOne.items = [1, 2, 9, 3, 3, 6];
